4021751c507278577c2b9c0ed945f1d7f6f3f89e
Novey Am <noveycea@gmail.com>
2025-08-18 12:44:44 -0700

    updated html header

--- sam_app/my-sam-app/myproject_django/asgi.py
+++ sam_app/my-sam-app/myproject_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for secure_share project.
+ASGI config for myproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secure_share.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject_django.settings')
 
 application = get_asgi_application()
